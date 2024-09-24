3261d761596c16a08c7f56f8c2abfbec4da0f9f1
Satyam Kumar <satyamkumarshahi3112@gmail.com>
2024-09-24 15:11:39 +0530

    Add files via upload

---
+++ OOPS/Lecture-24 September/basic_cass2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Point{
+    int x;
+    public:
+    // Point(int x1){
+    //     x = x1;
+    // }
+    void display()
+    {
+        cout<<"value of x :"<<x;
+    }
+    operator int()
+    {
+        return x;
+    }
+
+};
+int main()
+{
+    Point p1 = 10;
+    // int x = p1;
+
+    p1.display();
+    return 0;
+}
